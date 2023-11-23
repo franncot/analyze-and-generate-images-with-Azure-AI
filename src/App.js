8f5e6b8d9809504fa69c0a58c24860370bf14ee1
franncot <franco.toq@gmail.com>
2023-11-23 14:39:10 -0600

    Update Gui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import React, { useState } from 'react';
+
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [url, setUrl] = useState('');
+
+  const handleUrlChange = (event) => {
+    setUrl(event.target.value);
+  };
+
+  const handleActionClick = () => {
+    // Perform action logic here
+  };
+
+  const handleAnalyzeClick = () => {
+    // Perform analyze logic here
+  };
+
+  return (
+    <div>
+      <h1>Website Title</h1>
+      <input type="text" value={url} onChange={handleUrlChange} />
+      <button onClick={handleActionClick}>Action</button>
+      <button onClick={handleAnalyzeClick}>Analyze</button>
+    </div>
+  );
 }
 
 export default App;
